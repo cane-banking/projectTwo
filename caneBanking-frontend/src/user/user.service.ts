9d09b5513cf45a1ca8d7ee613247edf370580ce3
nilamp2101 <nilamp0505@gmail.com>
2021-01-27 16:46:38 -0600

    changed index.ts

---
+++ caneBanking-frontend/src/user/user.service.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { User } from './user';
+
+class UserService {
+    private URI: string;
+    constructor() {
+        // URL of the express server
+        this.URI = 'https://h2zckpset2.execute-api.us-west-2.amazonaws.com/default/login';
+    }
+    getLogin(): Promise<User> {
+        // withCredentials sends our cookies with the request.
+        return axios.get(this.URI).then(result=>{
+            console.log(result);
+            return result.data
+        });
+    }
+
+    login(user: User): Promise<User> {
+        console.log(user);
+        return axios.post(this.URI, user).then(result => result.data).catch(err => err);
+    }
+    logout(): Promise<null> {
+        return axios.delete(this.URI).then(result => null);
+    }
+}
+
+export default new UserService();
---
+++ expressbackend/src/lambda/login.ts
@@ -0,0 +1,52 @@
+import * as AWS from 'aws-sdk';
+
+
+let docClient = new AWS.DynamoDB.DocumentClient({
+    region: 'us-west-2',
+    endpoint: 'http://dynamodb.us-west-2.amazonaws.com'
+});
+
+//handler
+
+export const handler = async (event: any): Promise<any> => {
+    const user = await getUserByName(event.body.username);
+    console.log('ive been hit yikes');
+    if (user && user.password === event.body.password) {
+        return {statusCode: 200,  headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        }, body: JSON.stringify(user)};
+    } else {
+        return {statusCode: 404, body: JSON.stringify({})};
+    }
+}
+
+
+//service to talk to database
+async function getUserByName(username: string): Promise<User | null> {
+    const params = {
+        TableName: 'users',
+        Key: {
+            'username': username
+        }
+    };
+    return await docClient.get(params).promise().then((data) => {
+        if (data && data.Item) {
+            //logger.debug(`data.Item: ${JSON.stringify(data.Item)}`);
+            return data.Item as User;
+        } else {
+            return null;
+        }
+    })
+}
+
+export class User {
+    public role: string = 'customer';
+    constructor(public username: string, public firstname: string,public lastname: string,public password: string, role: string, public email: string ) {
+        if (role) {
+            this.role = role;
+        }
+    };
+}
