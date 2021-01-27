57fb5f1947b8e7d0f8b2051e62a81118661b23a7
Emily484 <63012821+Emily484@users.noreply.github.com>
2021-01-27 12:23:04 -0800

    signup form

--- caneBanking-frontend/src/user/signup.component.tsx
+++ caneBanking-frontend/src/user/signup.component.tsx
@@ -1,10 +1,72 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import style from '../../global-styles';
+import { loginAction } from '../store/actions';
+import { UserState } from '../store/store';
 
-export default function SignUpComponent() {
+interface SignupProp {
+    navigation: any;
+}
+
+export default function SignUpComponent({ navigation }: SignupProp) {
+    const userSelector = (state: UserState) => state.loginUser;
+    const user = useSelector(userSelector);
+    const dispatch = useDispatch();
+
+    function createAccount(){
+        return
+    }
+
     return (
-       <View>
-           <Text>Sign up for an account</Text>
+        <View style={[style.container, style.login]}>
+           <Text>sign up for an account</Text>
+            <TextInput
+                    placeholder='username'
+                    style={style.input}
+                    onChangeText={(value) =>
+                        // change loginAction
+                        dispatch(loginAction({ ...user, username: value }))
+                    }
+                    value={user.username}
+                />
+             <TextInput
+                    placeholder='password'
+                    style={style.input}
+                    onChangeText={(value) =>
+                        // change loginAction
+                        dispatch(loginAction({ ...user, username: value }))
+                    }
+                    value={user.password}
+                />
+             <TextInput
+                    placeholder='first name'
+                    style={style.input}
+                    onChangeText={(value) =>
+                        // change loginAction
+                        dispatch(loginAction({ ...user, username: value }))
+                    }
+                    value={user.username}
+                />
+             <TextInput
+                    placeholder='last name'
+                    style={style.input}
+                    onChangeText={(value) =>
+                        // change loginAction
+                        dispatch(loginAction({ ...user, username: value }))
+                    }
+                    value={user.username}
+                />
+             <TextInput
+                    placeholder='password'
+                    style={style.input}
+                    onChangeText={(value) =>
+                        // change loginAction
+                        dispatch(loginAction({ ...user, username: value }))
+                    }
+                    value={user.username}
+                />
+                <Button onPress={createAccount} title='Create Account' color='#63D4FF' />
        </View>
     )
 }
