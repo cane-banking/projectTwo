1524421cd969f313cfb752e5daa5b52c6282597b
Emily484 <63012821+Emily484@users.noreply.github.com>
2021-02-05 17:47:24 -0800

    updating with master

--- caneBanking-frontend/src/router/DrawerNavigation.component.tsx
+++ caneBanking-frontend/src/router/DrawerNavigation.component.tsx
@@ -2,17 +2,12 @@ import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import{HomeTabNavigatorComponent} from "./TabNavigator.component";
 import { enableScreens } from 'react-native-screens';
-import { LogoutStack, ApplicationStack, DepositCheckStack, TransactionStack, TransferStack, loginStackNavigator, OtherTransferStack, AccountStack } from "./MainStackNavigator.component";
+import { ApplicationStack, DepositCheckStack, TransactionStack, TransferStack, loginStackNavigator, OtherTransferStack, AccountStack } from "./MainStackNavigator.component";
 import Accounts from "../account/accounts.component";
 import { useDispatch, useSelector } from "react-redux";
 import { CaneBankingState } from "../store/store";
-import { changeUser } from "../store/actions";
-import { User } from "../user/user";
 import LoginComponent from "../user/login.component";
 import Logout from "../user/logout.component";
-import OtherTransfer from "../transaction/OtherTransfer";
-import { UserState } from "../store/store";
-import { useSelector } from "react-redux";
 
 
 
